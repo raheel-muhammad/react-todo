c4c70fc3eda60bfe615a58e626cd0b58a4621646
Makhdoom-Sharif <makhdomsharif@gmail.com>
2023-09-04 22:24:09 +0500

    Todo completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import "./App.css"
 export default function App() {
   const [inputvalue, setInputValue] = useState("")
   const [items, setItems] = useState([])
@@ -9,18 +10,25 @@ export default function App() {
   const handleInputChange = (event) => {
     setInputValue(event.target.value)
   }
+  const handleEnterKey = (event) => {
+    if (event.key === 'Enter' && inputvalue.trim() !== '') {
+      setItems([...items, inputvalue]);
+      setInputValue('');
+    }
+  }
   const handleClick = () => {
-    if (inputvalue.trim() === '') {
+    let item = inputvalue.trim()
+    if (!item) {
       alert('please fill out this field')
     } else {
       if (isEdit) {
-        setIsEdit(false)
         let tempArr = [...items]
-        tempArr[currIndex] = inputvalue.trim()
+        tempArr[currIndex] = item
         setItems(tempArr)
         setInputValue('')
+        setIsEdit(false)
       } else {
-        setItems((prevItems) => [...prevItems, inputvalue.trim()]);
+        setItems((prevItems) => [...prevItems, item]);
         setInputValue('')
       }
     }
@@ -35,40 +43,43 @@ export default function App() {
     let prev = [...items]
     prev.splice(index, 1)
     setItems(prev)
+
   }
   const resetItem = () => {
     setInputValue('')
     if (isEdit) {
       setIsEdit(false)
-    } else {
-      setIsEdit(true)
+      setCurrIndex()
     }
-    setIsEdit(false)
   }
 
   return (
     <>
       <div className='container'>
+        <div className="heading">Todo List</div>
         <div className='form'>
-          <input type="text" placeholder='Enter anything you want' value={inputvalue} onChange={handleInputChange} />
-          <button onClick={handleClick}>{isEdit ? "edit" : "add"}</button>
-          <button onClick={resetItem}>Cancel</button>
-
+          <input className='input' type="text" maxLength={100} placeholder='Enter anything you want' value={inputvalue} onChange={handleInputChange} onKeyDown={handleEnterKey} />
+          <div className='button-2'>
+            <button className='btn btn-add' onClick={handleClick}>{isEdit ? "Edit" : "Add"}</button>
+            <button className='btn btn-cancel' onClick={resetItem}>{isEdit ? 'Reset' : 'Cancel'}</button>
+          </div>
         </div>
 
-        <ul>
+
+        <div className='ul-list'>
 
           {items.map((item, index) => (
-            <div style={{ display: 'flex', flexDirection: 'row' }} >
+            <div className='setting' style={{ display: 'flex', justifyContent: 'space-between', width: '100%', marginTop: '20px', backgroundColor: 'antiquewhite', borderRadius: '4px' }} >
 
-              <li key={index}>{item}</li>
-              <button onClick={() => editClick(item, index)} style={{ color: 'red' }}>edit</button>
-              <button onClick={() => deleteItem(index)}>Delete</button>
+              <div className='main' key={index}>{item}</div>
+              <div className='main-btn'>
+                <button className="btn-edit" onClick={() => editClick(item, index)}>Edit</button>
+                <button className={isEdit ? "btn-l8grey" : "btn-delete"} onClick={() => deleteItem(index)} disabled={isEdit ? true : false} >Delete</button>
+              </div>
             </div>
           ))}
-        </ul>
+        </div>
       </div>
-      {/* <Home/> */}
     </>
   )
 }
