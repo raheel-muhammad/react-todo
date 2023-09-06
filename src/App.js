69fdcf273aea9a7903f3c0e73fe6e82c47195f74
Makhdoom-Sharif <makhdomsharif@gmail.com>
2023-09-06 23:11:17 +0500

    react addition

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ export default function App() {
         <form className='form' onSubmit={handleSubmit}>
           <input className='input' type="text" maxLength={100} placeholder='Enter anything you want' value={inputvalue} onChange={handleInputChange} />
           <div className='button-2'>
-            <button className={`${!inputvalue ? "light-grey" : "btn-add"}`} onClick={handleClick} disabled={disabled} type='submit'>{isEdit ? "Update" : "Add"}</button>
+            <button className={`${!inputvalue.trim().length ? "light-grey" : "btn-add"}`} onClick={handleClick} disabled={disabled} type='submit'>{isEdit ? "Update" : "Add"}</button>
             <button className={`${!inputvalue ? "btn-1" : "btn-cancel"}`} onClick={resetItem} type='button'>{isEdit ? 'Reset' : 'Cancel'}</button>
           </div>
         </form>
